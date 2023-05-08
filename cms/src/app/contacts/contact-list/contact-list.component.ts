9234390f9d1c3352836eeb89689ebea97f5f0c8b
marindrake <marindrake@byui.edu>
2023-05-08 00:38:37 -0600

    update

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
@@ -7,12 +7,18 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit{
+  constructor() {}
+
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact [] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu','208-496-3771','../../assets/images/jacksonk.jpg', null),
-    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null)  
+    new Contact('1', 'Brother Jackson', 'jacksonk@byui.edu','208-496-3771','../../assets/images/jacksonk.jpg', null),
+    new Contact('2', 'Brother Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null)  
   ];
 
-  constructor () {}
-    ngOnInit(){
-    }
+  ngOnInit(){}
+
+  onSelected(contact: Contact){
+    this.selectedContactEvent.emit(contact);
+  }
 }
